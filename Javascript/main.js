9f976101e5d158af76a5a19faab803711910b66a
LukesicoolDEV <lukeiscool2543@gmail.com>
2026-07-10 14:45:05 +0800

    update Javascript/main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -15,3 +15,25 @@ window.addEventListener('click', function(event) {
         infoModal.style.display = 'none';
     }
 });
+const infoBtn = document.querySelector('.info-btn');
+const infoModal = document.getElementById('infoModal');
+const closeBtn = document.querySelector('.close');
+const downloadBtn = document.querySelector('.download-btn');
+
+infoBtn.addEventListener('click', function() {
+    infoModal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function() {
+    infoModal.style.display = 'none';
+});
+
+downloadBtn.addEventListener('click', function() {
+    infoModal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == infoModal) {
+        infoModal.style.display = 'none';
+    }
+});
